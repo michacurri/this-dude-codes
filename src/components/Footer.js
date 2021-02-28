37d7ab18be478a9f49935c0b3c1fcd590086f5ff
michacurri <mkcurri@gmail.com>
2021-02-28 15:19:33 -0500

    updated packages; copyright 2021

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
         <li><a href="https://www.linkedin.com/in/mkcurrie/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a></li>
         <li><a href="https://twitter.com/thisdudecodes" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'twitter']} /></a></li>
       </ul>
-      <h5>&copy; thisdude.codes 2020</h5>
+      <h5>&copy; thisdude.codes 2021</h5>
     </footer>
   )
 }
